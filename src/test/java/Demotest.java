b6d85b95e97283d5aa98a1cb9ddd665f8cb12ced
emiola <ennymat25@gmail.com>
2021-08-07 12:55:15 +0100

    k

--- src/test/java/Demotest.java
+++ src/test/java/Demotest.java
@@ -1,16 +1,28 @@
 import Base.TestBase;
+import Listeners.ExtentReportListener;
+import Listeners.RetryListener;
 import io.qameta.allure.Description;
 import io.qameta.allure.Severity;
 import io.qameta.allure.SeverityLevel;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 import static utility.BrokenLink.BrokenLink;
+import static utility.ExcelUtil.DoClick;
 
+@Listeners({ExtentReportListener.class, RetryListener.class})
 public class Demotest extends TestBase {
 
     @Description("login")
     @Severity(SeverityLevel.NORMAL)
     @Test(priority = 2)
     public void logina() throws Exception {
-        BrokenLink();
+        //BrokenLink();
+    }
+
+    @Description("login")
+    @Severity(SeverityLevel.NORMAL)
+    @Test(priority =3)
+    public void login() throws Exception {
+        DoClick("lbtn_XPATH", 5);
     }
 }
